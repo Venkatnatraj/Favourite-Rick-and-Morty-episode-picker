d8b784307d323bb046ec257cd5ad0aade7ba8f9a
Venkatnatraj <t.venkatanatraj95@gmail.com>
2020-04-25 20:31:55 +0530

    Initialise useReducer

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './App.css';
 import { store } from './Store';
 
 function App(): JSX.Element {
-  const data = useContext(store);
+  const { state, dispatch } = useContext(store);
+
+  useEffect(()=>{
+    state.episodes.length === 0 && fetchData()
+  })
+
+  const fetchData = async () => {
+    const url = "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
+    const data = await fetch(url);
+    const jsonData = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: jsonData._embedded.episodes
+    })
+  }
   return (
     <div>
+      {console.log(state)}
       <h1>Rick and Morty</h1>
-      {console.log(data.episodes)}
     </div>
   );
 }
--- my-app-ts/src/Store.tsx
+++ my-app-ts/src/Store.tsx
@@ -1,17 +1,33 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 interface IState {
     episodes: [],
     favourites: []
+};
+
+interface IAction {
+    type: string,
+    payload: any
 }
 
-const initialState: IState = {episodes: [], favourites:[]};
+const initialState: IState = { episodes: [], favourites: [] };
 
-export const store = createContext<IState>(initialState);
+export const store = createContext<IState | any>(initialState);
 
-export const StoreProvider = (props: any)=> {
+const reducer = (state: IState, action: IAction): IState => {
+    switch (action.type) {
+        case "FETCH_DATA":
+            return { ...state, episodes: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const StoreProvider = (props: any): JSX.Element => {
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
-        <store.Provider value={initialState}>
+        <store.Provider value={{state, dispatch}}>
             {props.children}
         </store.Provider>
     )
